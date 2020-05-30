21e042f035cc191fcf9a89a399f3a800a434c795
Nairmangit <58293361+Nairmangit@users.noreply.github.com>
2020-05-30 21:59:16 +0500

    Update send.py

--- send.py
+++ send.py
@@ -1,10 +1,9 @@
-import requests
-import datetime
+import requests, datetime, random
 
 urlc = 'https://nairman.pythonanywhere.com'
 urls = 'https://nairman.pythonanywhere.com/send'
 client = requests.session()
 client.get(urlc)
 csrftoken = client.cookies['csrftoken']
-dd = dict(tem=123,dat=datetime.datetime.now(),sensfk=1,csrfmiddlewaretoken=csrftoken)
+dd = dict(tem=random.uniform(-100,100),dat=datetime.datetime.now(),sensfk=1,csrfmiddlewaretoken=csrftoken)
 client.post(urls, data = dd)
